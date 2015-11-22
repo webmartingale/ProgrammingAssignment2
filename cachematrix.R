a223f97afd5a9db22b9a6b1c4e7c729c36978751
webmartingale <webmartingale@gmail.com>
2015-11-22 23:54:24 +0100

    Added comments to R Programming / PA2

--- cachematrix.R
+++ cachematrix.R
@@ -20,19 +20,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function returns the inverse of the matrix stored in a cacheMatrix
+## This function returns the inverse of the matrix stored in a cacheMatrix x
 ## if the inverse is not yet already computed and stored in cache
 ## then it's calculated on the spot and cached before returning
 ## otherwise it return the pre-cached inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    #check if already cached
     inv <- x$getInv()
     if (!is.null(inv))
     {
         message("getting cached matrix inverse from cache.")
         return(inv)
     }
+
+    #otherwise calculate inverse and cache
     inv <- solve(x$getData())
     x$setInv(inv)
+
+    ## Return a matrix that is the inverse of 'x'
     inv
 }
